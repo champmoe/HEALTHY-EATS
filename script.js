521070e5cafd5000a15219637ec755024d57dcfa
champmoe <mali@westinghousehs.org>
2018-12-06 09:55:46 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$("#first").click(function() {
+    $("#para1").slideDown();
+});
+
+$("#second").click(function() {
+    $("#para2").slideDown();
+});
+
+$("#third").click(function() {
+    $("#para3").slideDown();
+});
+
+$("#fourth").click(function() {
+    $("#para4").slideDown();
+});
